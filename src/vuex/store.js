d8faa7a067a185b5aeb6b370c71ee0a5635d296e
Scofieldsu <931180361@qq.com>
2017-08-08 18:03:13 +0800

    update port

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -9,13 +9,13 @@ Vue.use(Vuex)
 const state = {
   count: 10,
   centerRightWidth: 230,
-  api_url: 'http://192.168.1.230:5050/api',
+  api_url: 'http://192.168.1.230/api',
   gitlab_info: {
     gitlaburl: 'http://192.168.5.10',
     path: '/oauth/authorize?',
     params: {
       client_id: 'a164506151fbd065a97d3cd9654ee0e37f80ffe81616a8311c320022a097e2c3',
-      redirect_uri: 'http://192.168.1.230:5050/login/gitlab',
+      redirect_uri: 'http://192.168.1.230/login/gitlab',
       response_type: 'code',
       state: 'oneplatform'
     }
