618e2fccd8a39ccb3f31f6232d90f605dd6c96ae
Scofieldsu <931180361@qq.com>
2017-09-06 17:18:42 +0800

    add public info

--- src/components/NavForTools/Markdown.vue
+++ src/components/NavForTools/Markdown.vue
@@ -1,17 +1,17 @@
 <template>
     <div>
-      <el-input v-model="title" placeholder="标题" style="margin: 20px 0;width: 80%"></el-input>
+      <el-input v-model="title" placeholder="标题"  type="textarea" :autosize={minRows:1,maxRows:10} style="margin: 20px 0;width: 80%"></el-input>
       <div class="editor-container" style="width: 80%;overflow:auto">
         <md-editor id='contentEditor' ref="contentEditor" v-model='content' :height="600" :zIndex='20'></md-editor>
       </div>
       <div style="margin-top: 50px">
         <el-button type="primary" @click="handlesubmit">发布</el-button>
-        <el-button>取消</el-button>
       </div>
     </div>
 </template>
 
 <script>
+  import { mapGetters } from 'vuex'
   import MdEditor from './Mdeditor.vue'
   import ElInput from '../../../node_modules/element-ui/packages/input/src/input'
   import ElButton from '../../../node_modules/element-ui/packages/button/src/button'
@@ -20,6 +20,11 @@
       ElButton,
       ElInput,
       MdEditor},
+    computed: {
+      ...mapGetters([
+        'getApiUrl'
+      ])
+    },
     data () {
       return {
         title: '',
@@ -28,7 +33,89 @@
     },
     methods: {
       handlesubmit () {
-        console.log(typeof (this.content))
+        let userid = window.localStorage.getItem('user_id')
+        if (userid) {
+          userid = Number(userid)
+        }
+        if (!userid) {
+          this.$notify({
+            title: '无效的用户',
+            type: 'error',
+            duration: 1200
+          })
+        } else if (!this.title) {
+          this.$notify({
+            title: '标题不能为空！',
+            type: 'error',
+            duration: 1200
+          })
+        } else if (!this.content) {
+          this.$notify({
+            title: '内容不能为空！',
+            type: 'error',
+            duration: 1200
+          })
+        } else {
+          let resourse = {
+            'jsonrpc': '2.0',
+            'method': 'publicinfoapi.add_public_info',
+            'id': 1111,
+            'params': {
+              'user_id': userid,
+              'title': this.title,
+              'content': this.content
+            }
+          }
+          this.axios.post(this.getApiUrl, resourse)
+            .then((res) => {
+              console.log(res)
+              if (res.data !== '' && 'result' in res.data) {
+                if ('msg' in res.data.result) {
+                  if (res.data.result.msg === 'success') {
+                    this.$notify({
+                      title: 'Add PublicInfo Success',
+                      type: 'success',
+                      duration: 1200
+                    })
+                    this.title = ''
+                    this.content = ''
+                    this.$router.push('/yours/notices')
+                  } else {
+                    let msg = res.data.result.msg
+                    this.$notify({
+                      title: 'Add PublicInfo Failed',
+                      message: msg,
+                      type: 'error',
+                      duration: 1200
+                    })
+                  }
+                }
+              } else if ('error' in res.data) {
+                let error = res.data.error
+                this.$notify({
+                  title: 'Add PublicInfo Failed',
+                  message: error,
+                  type: 'error',
+                  duration: 1200
+                })
+              } else {
+                this.$notify({
+                  title: 'Add PublicInfo Failed',
+                  message: 'Some abnormal error has happened!',
+                  type: 'error',
+                  duration: 1200
+                })
+              }
+            })
+            .catch((err) => {
+              console.error(err)
+              this.$notify({
+                title: 'Add PublicInfo Failed',
+                type: 'error',
+                duration: 1200
+              })
+            })
+        }
       }
     }
   }
--- src/components/NavForTools/ShowNotice.vue
+++ src/components/NavForTools/ShowNotice.vue
@@ -8,7 +8,7 @@
         </span>
       </div>
       <div style="margin-top: 20px">
-        <el-alert v-for="notice in noticeData" :key="notice.id"
+        <el-alert v-for="notice in noticeData" :key="notice.time"
           @close="handle_close(notice.id)" title="" style="margin-top: 10px;background-color: lightcoral">
           <span v-show="false">{{notice.id}}</span>
           <span style="color: #333333;font-weight: bold">{{notice.user_name}}</span>
@@ -26,45 +26,54 @@
           <label style="font-size: large;font-weight: bolder">公告</label>
         </span>
       </div>
-      <el-card class="box-card" v-for="o in 10" :key="o">
+      <el-card class="box-card" v-for="pubinfo in publicData " :key="pubinfo.title">
         <div  class="text item">
-          <i style="font-weight: bolder;color: #ff4949">{{'公告标题'}}</i>
+          <i style="font-weight: bolder;color: #ff4949">{{pubinfo.title}}</i>
           <br/>
-          {{'公告内容' + o }}
+          {{pubinfo.summary }} <i style="font-weight: bolder">......</i>
           <br/>
-          <el-button type="text" style="float: right">展开</el-button>
+          <el-button type="text" style="float: right" @click="showdetail(pubinfo.id)">展开</el-button>
           <br/>
-          <i style="font-size: small;font-weight: lighter;color: #324157;">{{'公告作者'}}</i>
-          <i style="font-size: small;font-weight: lighter;color: lightseagreen; margin-left: 5px">{{'2017-09-05'}}</i>
+          <i style="font-size: small;font-weight: lighter;color: #324157;">{{pubinfo.user}}</i>
+          <i style="font-size: small;font-weight: lighter;color: lightseagreen; margin-left: 5px">{{pubinfo.time}}</i>
+          <br/>
+          <i v-if="!(pubinfo.time===pubinfo.update_time)" style="font-size: small;font-weight: lighter;color: #ff6d6d;">update by</i>
+          <i v-if="!(pubinfo.time===pubinfo.update_time)" style="font-size: small;font-weight: lighter;color: #324157;">{{pubinfo.update_user}}</i>
+          <i v-if="!(pubinfo.time===pubinfo.update_time)" style="font-size: small;font-weight: lighter;color: lightseagreen; margin-left: 5px">{{pubinfo.update_time}}</i>
         </div>
       </el-card>
+      <el-dialog title="公告详情" :visible.sync="dialogTableVisible">
+        <el-input v-model="id"  v-show="false" style="margin: 20px 0;"></el-input>
+        <el-input v-model="title" placeholder="标题"  type="textarea" :autosize={minRows:1,maxRows:10} style="margin: 20px 0;"></el-input>
+        <div class="editor-container" style="overflow:auto">
+          <md-editor id='contentEditor' ref="contentEditor" v-model='content' :height="600" :zIndex='20'></md-editor>
+        </div>
+            <el-button type="primary" @click="update_pubinfo">提交修改</el-button>
+            <el-button @click="cancel">取消</el-button>
+           <el-button type="danger" style="float: right" @click="handleDelete(this.id)">删除</el-button>
+      </el-dialog>
     </div>
   </div>
 </template>
 
 <script>
   import { mapGetters } from 'vuex'
+  import MdEditor from './Mdeditor.vue'
   import ElButton from '../../../node_modules/element-ui/packages/button/src/button'
   import ElAlert from '../../../node_modules/element-ui/packages/alert/src/main'
   export default {
     components: {
       ElAlert,
+      MdEditor,
       ElButton},
     data () {
       return {
-        proclamation: '',
+        id: '',
+        title: '',
+        content: '',
         input_select: '',
         select: '',
         dialogTableVisible: false,
-        editform: {
-          id: 0,
-          name: '',
-          shortcut: '',
-          tag: '',
-          notice: true,
-          link: '',
-          desc: ''
-        },
         resultData: [
           {
             tag: 'default',
@@ -72,7 +81,8 @@
             name: 'service_name'
           }
         ],
-        noticeData: []
+        noticeData: [],
+        publicData: []
       }
     },
     computed: {
@@ -114,8 +124,203 @@
         .then(function (res) {
           that.noticeData = res.data.result
         })
+      let respublic = {
+        'jsonrpc': '2.0',
+        'method': 'publicinfoapi.get_public_info_list',
+        'id': 1111,
+        'params': {
+          'user_id': userid
+        }
+      }
+      that.axios.post(this.getApiUrl, respublic)
+        .then(function (res) {
+          that.publicData = res.data.result
+        })
     },
     methods: {
+      showdetail (pubinfoid) {
+        let userid = window.localStorage.getItem('user_id')
+        if (userid) {
+          userid = Number(userid)
+        }
+        let resourse = {
+          'jsonrpc': '2.0',
+          'method': 'publicinfoapi.get_public_info',
+          'id': 1111,
+          'params': {
+            'user_id': userid,
+            'pubinfo_id': pubinfoid
+          }
+        }
+        this.axios.post(this.getApiUrl, resourse)
+          .then((res) => {
+            this.id = res.data.result.id
+            this.title = res.data.result.title
+            this.content = res.data.result.content
+            this.dialogTableVisible = true
+          })
+          .catch(function (err) {
+            console.log(err)
+          })
+      },
+      update_pubinfo () {
+        let userid = window.localStorage.getItem('user_id')
+        if (userid) {
+          userid = Number(userid)
+        }
+        if (!userid) {
+          this.$notify({
+            title: '无效的用户',
+            type: 'error',
+            duration: 1200
+          })
+        } else if (!this.title) {
+          this.$notify({
+            title: '标题不能为空',
+            type: 'error',
+            duration: 1200
+          })
+        } else if (!this.content) {
+          this.$notify({
+            title: '内容不能为空',
+            type: 'error',
+            duration: 1200
+          })
+        } else {
+          let resourse = {
+            'jsonrpc': '2.0',
+            'method': 'publicinfoapi.update_public_info',
+            'id': 1111,
+            'params': {
+              'user_id': userid,
+              'pubinfo_id': this.id,
+              'title': this.title,
+              'content': this.content
+            }
+          }
+          this.axios.post(this.getApiUrl, resourse)
+            .then((res) => {
+              if (res.data !== '' && 'result' in res.data) {
+                if ('msg' in res.data.result) {
+                  if (res.data.result.msg === 'success') {
+                    this.$notify({
+                      title: 'Update Public info Success',
+                      type: 'success',
+                      duration: 1200
+                    })
+                  } else {
+                    let msg = res.data.result.msg
+                    this.$notify({
+                      title: 'Update Public info Failed',
+                      message: msg,
+                      type: 'error',
+                      duration: 1200
+                    })
+                  }
+                }
+              } else if ('error' in res.data) {
+                let error = res.data.error
+                this.$notify({
+                  title: 'Update Public info Failed',
+                  message: error,
+                  type: 'error',
+                  duration: 1200
+                })
+              } else {
+                this.$notify({
+                  title: 'Update Public info Failed',
+                  message: 'Some abnormal error has happened!',
+                  type: 'error',
+                  duration: 1200
+                })
+              }
+            })
+            .catch((err) => {
+              console.error(err)
+              this.$notify({
+                title: 'Update Public info Failed',
+                type: 'error',
+                duration: 1200
+              })
+            })
+        }
+        this.dialogTableVisible = false
+      },
+      handleDelete () {
+        this.$confirm('此操作将永久删除该公告, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          let that = this
+          let userid = window.localStorage.getItem('user_id')
+          if (userid) {
+            userid = Number(userid)
+          }
+          let resourse = {
+            'jsonrpc': '2.0',
+            'method': 'publicinfoapi.delete_public_info',
+            'id': 1111,
+            'params': {
+              'user_id': userid,
+              'pubinfo_id': this.id
+            }
+          }
+          that.axios.post(this.getApiUrl, resourse)
+            .then((res) => {
+              if (res.data !== '' && 'result' in res.data) {
+                if ('msg' in res.data.result) {
+                  if (res.data.result.msg === 'success') {
+                    this.dialogTableVisible = false
+                    this.$message({
+                      type: 'success',
+                      message: 'Delete Public info Success'
+                    })
+                  } else {
+                    let msg = res.data.result.msg
+                    this.$notify({
+                      title: 'Delete Delete Public info Success Failed',
+                      message: msg,
+                      type: 'error',
+                      duration: 1200
+                    })
+                  }
+                }
+              } else if ('error' in res.data) {
+                let error = res.data.error
+                this.$notify({
+                  title: 'Delete Delete Public info Success Failed',
+                  message: error,
+                  type: 'error',
+                  duration: 1200
+                })
+              } else {
+                this.$notify({
+                  title: 'Delete Delete Public info Success Failed',
+                  message: 'Some abnormal error has happened!',
+                  type: 'error',
+                  duration: 1200
+                })
+              }
+            })
+            .catch((err) => {
+              console.error(err)
+              this.$notify({
+                title: 'Delete Delete Public info Success Failed',
+                type: 'error',
+                duration: 1200
+              })
+            })
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          })
+        })
+      },
+      cancel () {
+        this.dialogTableVisible = false
+      },
       handle_close (noticeid) {
         console.log(noticeid)
         let userid = window.localStorage.getItem('user_id')
